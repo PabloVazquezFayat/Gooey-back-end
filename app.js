a8243475e3a4d18133a4a2c944eff4590467d216
Pablo Vazquez <pvazqez@vandldigital.com>
2019-08-20 00:13:44 -0400

    more back-end setup

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-
-
-const indexRouter = require('./routes/index');
 
 const app = express();
 
@@ -24,11 +22,13 @@ mongoose
 
 // setup middlewares
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes 
+const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -14,5 +14,29 @@ router.get('/', async (req, res, next)=> {
   }
 });
 
+router.get('/movie/:id', async (req, res, next)=> {
+  try{
+    let movie = await Movie.findById(req.params.id);
+    res.status(200).json(movie);
+  }catch(error){
+    next((error)=>{
+      console.log(error);
+    });
+  }
+});
+
+
+//this route can be refactor for creating new DB entries
+router.post('/movie', async (req, res, next)=> {
+  try{
+    res.status(200).json(req.body)
+    console.log(req.body);
+  }catch(error){
+    next((error)=>{
+      console.log(error);
+    });
+  }
+});
+
 
 module.exports = router;
